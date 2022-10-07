cb13f9bdd4f89320d1e917c5d746267fdc78d37d
Sujila K <sujilak@blueehr.com>
2022-10-07 18:48:26 +0530

    Changed home link lables

--- client/src/App.js
+++ client/src/App.js
@@ -17,9 +17,9 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello!!!!!
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Home Page</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
